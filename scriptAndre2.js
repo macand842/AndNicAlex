f085f42f6e7226e622b8adef74dc0e8f012ca53c
Andre Mackus <macand842@gmx.net>
2023-09-12 12:24:40 +0200

    opgave 2 og 3

---
+++ scriptAndre2.js
@@ -0,0 +1,14 @@
+// Prompt the user for input
+const number = parseInt(prompt("Enter a number:"));
+let num = Math.abs(number);
+let sum = 0;
+
+// Loop to extract and sum the digits
+while (num > 0) {
+    const digit = num % 10; // Get the last digit
+    sum += digit; // Add the digit to the sum
+     num = Math.floor(num / 10); // Remove the last digit
+}
+
+// Display the sum in the document
+document.write(`The sum of digits in ${number} is: ${sum}`);
---
+++ scriptAndre3.js
@@ -0,0 +1,18 @@
+function generatePattern() {
+  const numRowsInput = document.getElementById("numRows");
+  const patternDiv = document.getElementById("pattern");
+  patternDiv.innerHTML = ""; // Clear any previous pattern
+
+  const numRows = parseInt(numRowsInput.value);
+
+      let pattern;
+
+      for (let i = 1; i <= numRows; i++) {
+          for (let j = 1; j <= i; j++) {
+              pattern += "*";
+          }
+          pattern += "<br>"; // Add a line break for the next row
+      }
+
+      patternDiv.innerHTML = pattern;
+  }
