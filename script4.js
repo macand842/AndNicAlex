393a193e400d9d2fce40e29505d6bd7db2bbd5c0
NDODEV <nielsen6655@gmail.com>
2023-09-12 12:35:46 +0200

    Tilføjet kommentare

--- script4.js
+++ script4.js
@@ -1,12 +1,19 @@
-let num1, num2, randomOperation, result;
+// Deklarerer variabler til senere brug
+let num1, num2, randomOperationValg, result;
 
+// Funktion til at udføre en tilfældig matematisk operation
 function forVoresUdregning() {
+  // Genererer to tilfældige tal mellem 1 og 10 (+ 1)
   num1 = Math.floor(Math.random() * 10) + 1;
   num2 = Math.floor(Math.random() * 10) + 1;
 
+  // En liste i et array med mulige matematiske operationer
   const operations = ['+', '-', '*', '/', '%', '**'];
+  
+  // Vælger tilfældigt en af operationerne fra arrayet
   randomOperationValg = operations[Math.floor(Math.random() * operations.length)];
 
+  // Udfører den valgte operation og gemmer resultatet i 'result' variablen
   switch (randomOperationValg) {
     case '+':
       result = num1 + num2;
@@ -28,6 +35,7 @@ function forVoresUdregning() {
       break;
   }
 
+  // Opdaterer HTML-elementer med de genererede tal og resultatet
   const taskElement = document.getElementById('tal1');
   taskElement.textContent = `Tal 1: ${num1}`;
   const taskElement1 = document.getElementById('tal2');
@@ -36,15 +44,20 @@ function forVoresUdregning() {
   taskElement2.textContent = ` Resultat: ${result}`;
 }
 
+// Funktion for at tjekke brugerens gæt
 function tjekGæt() {
+  // Henter brugerens gæt fra input-elementet med id 'brugerGæt'
   const userGuess = document.getElementById('brugerGæt').value;
   const resultTextElement = document.getElementById('resultatTekst');
   
+  // Sammenligner brugerens gæt med den tilfældige operation
   if (userGuess === randomOperationValg) {
     resultTextElement.textContent = 'Dit gæt er korrekt!';
   } else {
+    // Hvis gættet er forkert, vises det korrekte svar
     resultTextElement.textContent = `Dit gæt er forkert. Det korrekte svar var ${randomOperationValg}`;
   }
 }
 
+// Kalder funktionen 'forVoresUdregning' 
 forVoresUdregning();
